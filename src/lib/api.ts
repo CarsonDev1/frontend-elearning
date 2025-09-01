94a59998e82ed76ed90d9e8b64642e805cd5e3fe
CarsonDev1 <buitritinht@gmail.com>
2025-09-01 13:01:07 +0700

    uploadd code

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for API
-export const API_BASE_URL = 'https://14.225.205.107:8082/api';
+export const API_BASE_URL = 'https://14.225.205.107/api';
 
 // Create an axios instance with custom config
 const api = axios.create({
